8e775766a7a56055400ca6649eb826ac4f6d1592
aaronwai <aaron.lung.wai@gmail.com>
2024-01-29 21:54:53 +0800

    Step 2 : navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import { Urbanist } from "next/font/google";
 
 import "./globals.css";
-
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
 const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
