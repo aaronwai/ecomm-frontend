3587a1b834a563be589d93dfa5857809bf7c1e79
aaronwai <aaron.lung.wai@gmail.com>
2024-01-29 22:33:12 +0800

    Step 3 : billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,21 @@
+import Container from "@/components/ui/container";
+import Billboard from "@/components/ui/billboard";
+import getBillboard from "@/actions/get-billboard";
+
+export const revalidate = 0;
+
 const HomePage = async () => {
-  return <div className='space-y-10 pb-10'>home page</div>;
+  const billboard = await getBillboard("46bf3541-28c9-4e16-95c1-66926ed7035e");
+  return (
+    <Container>
+      <div className='space-y-10 pb-10'>
+        <Billboard data={billboard} />
+        <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+          {/* <ProductList title='Featured Products' items={products} /> */}
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -2,8 +2,19 @@
 
 import Button from "@/components/ui/button";
 import { ShoppingBag } from "lucide-react";
+import { useState, useEffect } from "react";
 
 const NavbarActions = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className='ml-auto flex items-center gap-x-4'>
       <Button
