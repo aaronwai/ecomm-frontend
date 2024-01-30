ebcc32e653cb4ba478bbef6caf6176fec1176b5d
aaronwai <aaron.lung.wai@gmail.com>
2024-01-30 20:48:11 +0800

    Step 7 : modal provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
+
 const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata = {
@@ -18,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
