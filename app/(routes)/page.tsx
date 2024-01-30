45e403973b5c8e4010c47e37e09cddbe1c4352c0
aaronwai <aaron.lung.wai@gmail.com>
2024-01-30 12:15:44 +0800

    Step 4 : product-card

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,17 +1,19 @@
 import Container from "@/components/ui/container";
 import Billboard from "@/components/ui/billboard";
 import getBillboard from "@/actions/get-billboard";
-
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 export const revalidate = 0;
 
 const HomePage = async () => {
   const billboard = await getBillboard("46bf3541-28c9-4e16-95c1-66926ed7035e");
+  const products = await getProducts({ isFeatured: true });
   return (
     <Container>
       <div className='space-y-10 pb-10'>
         <Billboard data={billboard} />
         <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
-          {/* <ProductList title='Featured Products' items={products} /> */}
+          <ProductList title='Featured Products' items={products} />
         </div>
       </div>
     </Container>
